6dadff508b99b924979105093cba72ab485e8b6b
Andrew huggins <andrewhuggins@gmail.com>
2016-09-04 17:37:10 -0400

    adding provider to register admin views

---
+++ src/Providers/ArtisanCMSAdminViewsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ArtisanCMS\AdminViews\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanCMSAdminViewsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views/admin', 'admin');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
